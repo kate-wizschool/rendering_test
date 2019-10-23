c13e234749876134705473105dcdeedf12f0f930
kate-wizschool <kate@wizschool.io>
2019-10-23 15:54:26 +0900

    commit init

--- next_test/.next/static/runtime/webpack.js
+++ next_test/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "131142ade72685dbda96";
+/******/ 	var hotCurrentHash = "a6326f1a1e7643f654c4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
